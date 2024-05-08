605fb21d8e394e48c118ec733cc3df4db933e0aa
Shohei Maeda <11495867+smaeda-ks@users.noreply.github.com>
2024-05-08 13:18:31 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -20,6 +20,7 @@ module.exports = withSentryConfig(
 
             config.experiments = {
                 asyncWebAssembly: true,
+                layers: true,
             };
 
             return config;
